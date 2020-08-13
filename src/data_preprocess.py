146fe82e1b2a0d5396e77998a04117786a79a2a9
yusanshi <meet.leiyu@gmail.com>
2020-08-13 22:28:10 +0800

    Faster way to generate word embedding

--- src/data_preprocess.py
+++ src/data_preprocess.py
@@ -271,32 +271,36 @@ def generate_word_embedding(source, target, word2int_path):
         word2int_path: vocabulary file when words in it will be searched in pretrained embedding file
     """
     # na_filter=False is needed since nan is also a valid word
-    word2int = dict(
-        pd.read_table(word2int_path, na_filter=False).values.tolist())
+    # word, int
+    word2int = pd.read_table(word2int_path, na_filter=False, index_col='word')
     source_embedding = pd.read_table(source,
                                      index_col=0,
                                      sep=' ',
                                      header=None,
-                                     quoting=csv.QUOTE_NONE)
-    target_embedding = np.random.normal(size=(1 + len(word2int),
-                                              Config.word_embedding_dim))
-    target_embedding[0] = 0
-    word_missed = 0
-    with tqdm(total=len(word2int),
-              desc="Generating word embedding from pretrained embedding file"
-              ) as pbar:
-        for k, v in word2int.items():
-            if k in source_embedding.index:
-                target_embedding[v] = source_embedding.loc[k].tolist()
-            else:
-                word_missed += 1
+                                     quoting=csv.QUOTE_NONE,
+                                     names=range(config.word_embedding_dim))
+    # word, vector
+    source_embedding.index.rename('word', inplace=True)
+    # word, int, vector
+    merged = word2int.merge(source_embedding,
+                            how='inner',
+                            left_index=True,
+                            right_index=True)
+    merged.set_index('int', inplace=True)
 
-            pbar.update(1)
+    missed_index = np.setdiff1d(np.arange(len(word2int) + 1),
+                                merged.index.values)
+    missed_embedding = pd.DataFrame(data=np.random.normal(
+        size=(len(missed_index), config.word_embedding_dim)))
+    missed_embedding['int'] = missed_index
+    missed_embedding.set_index('int', inplace=True)
+
+    final_embedding = pd.concat([merged, missed_embedding]).sort_index()
+    np.save(target, final_embedding.values)
 
     print(
-        f'Rate of word missed in pretrained embedding: {word_missed/len(word2int):.4f}'
+        f'Rate of word missed in pretrained embedding: {(len(missed_index)-1)/len(word2int):.4f}'
     )
-    np.save(target, target_embedding)
 
 
 def transform_entity_embedding(source, target, entity2int_path):
